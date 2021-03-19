7d57d79a577ff3358434c9d6819f4a4982c056a6
Sergiu <you@example.com>
2021-03-19 15:42:46 +0200

    Styling navigation and added settings btn

--- src/App.js
+++ src/App.js
@@ -1,90 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Components
+import Navigation from "./Components/Navigation/Navigation";
 
 // Pages
-import Trending from "./Components/Trending";
-import Newest from "./Components/Newest";
-import Upcoming from "./Components/Upcoming";
-import Discover from "./Components/Discover";
-import Watchlist from "./Components/Watchlist";
+import Trending from "./Components/Pages/Trending";
+import Newest from "./Components/Pages/Newest";
+import Upcoming from "./Components/Pages/Upcoming";
+import Discover from "./Components/Pages/Discover";
+import Watchlist from "./Components/Pages/Watchlist";
 
 // Material Ui
-import PropTypes from "prop-types";
-import AppBar from "@material-ui/core/AppBar";
-import Divider from "@material-ui/core/Divider";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-} from "@material-ui/core";
-import Hidden from "@material-ui/core/Hidden";
-
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-
-// === Icons ===
-import TheatersIcon from "@material-ui/icons/Theaters";
-import TrendingUpIcon from "@material-ui/icons/TrendingUp";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import MovieFilterIcon from "@material-ui/icons/MovieFilter";
-import StarIcon from "@material-ui/icons/Star";
-
-const useStyles = makeStyles({
-  drawerPaper: { width: "inherit" },
-});
 
 function App() {
-  const classes = useStyles();
-  const navTabs = ["trending", "newest", "discover", "watchlist"];
-  const navTabsIcons = [
-    <TheatersIcon />,
-    <TrendingUpIcon />,
-    <ArrowUpwardIcon />,
-    <MovieFilterIcon />,
-    <StarIcon />,
-  ];
-
-  const [openNav, setOpenNav] = useState(true);
-  const [activeTab, setActiveTab] = useState(navTabs);
-
-  const handleActiveTab = (e) => {
-    console.log(e.target);
-  };
-
   return (
     <div className="App">
-      <Router forceRefresh={false}>
-        <Drawer
-          // TODO For mobile, remove width
-          style={openNav ? { width: "240px" } : { width: "0px" }}
-          variant="persistent"
-          anchor="left"
-          open={openNav}
-          classes={{ paper: classes.drawerPaper }}
-        >
-          {navTabs.map((tab, index) => {
-            return (
-              <Link to={`/${tab}`} key={index}>
-                <List>
-                  <ListItem button onClick={() => handleActiveTab}>
-                    <ListItemIcon>{navTabsIcons[index]}</ListItemIcon>
-                    <ListItemText
-                      primary={tab}
-                      style={{
-                        textTransform: "capitalize",
-                        fontWeight: activeTab ? "bold" : "light",
-                      }}
-                    />
-                  </ListItem>
-                </List>
-              </Link>
-            );
-          })}
-          ;
-        </Drawer>
+      <Router>
+        <Navigation />
 
         <main>
           <Switch>
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import "./NavigationStyles.css";
+import classNames from "classnames/bind";
+
+// Components
+import SettingsTab from "./SettingsTab";
+
+// Router
+
+import { Link } from "react-router-dom";
+
+//Material-Ui
+
+import {
+  Drawer,
+  Divider,
+  Hidden,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+// === Icons ===
+import TheatersIcon from "@material-ui/icons/Theaters";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import MovieFilterIcon from "@material-ui/icons/MovieFilter";
+import StarIcon from "@material-ui/icons/Star";
+import SettingsIcon from "@material-ui/icons/Settings";
+
+const useStyles = makeStyles({
+  drawerPaper: {
+    width: "inherit",
+    backgroundColor: "transparent",
+  },
+  icon: {
+    fill: "#fff",
+  },
+});
+
+export default function Navigation() {
+  const classes = useStyles();
+  const navTabs = ["trending", "newest", "upcoming", "discover", "watchlist"];
+  const navTabsIcons = [
+    <TheatersIcon className={classes.icon} />,
+    <TrendingUpIcon className={classes.icon} />,
+    <ArrowUpwardIcon className={classes.icon} />,
+    <MovieFilterIcon className={classes.icon} />,
+    <StarIcon className={classes.icon} />,
+  ];
+
+  const [openNav, setOpenNav] = useState(true);
+  const [activeTab, setActiveTab] = useState(0);
+  const [settings, setSettings] = useState(true);
+
+  const handleActiveTab = (i) => {
+    setActiveTab(i);
+  };
+
+  const handleMouseEnter = () => {
+    setSettings(true);
+  };
+
+  const handleMouseLeave = () => {
+    setSettings(false);
+  };
+
+  return (
+    <Drawer
+      // TODO For mobile, remove width
+      style={openNav ? { width: "240px" } : { width: "0px" }}
+      variant="persistent"
+      anchor="left"
+      open={openNav}
+      classes={{ paper: classes.drawerPaper }}
+    >
+      <img src="../../../img/logo_transparent.png" alt="logo" />
+      <List>
+        {navTabs.map((tab, index) => {
+          const activeTabStyle = classNames("list-item", {
+            active: activeTab === index,
+          });
+
+          return (
+            <Link to={`/${tab}`} key={index}>
+              <ListItem button onClick={() => handleActiveTab(index)}>
+                <ListItemIcon>{navTabsIcons[index]}</ListItemIcon>
+                <ListItemText primary={tab} className={activeTabStyle} />
+              </ListItem>
+            </Link>
+          );
+        })}
+        <SettingsTab />
+      </List>
+    </Drawer>
+  );
+}
