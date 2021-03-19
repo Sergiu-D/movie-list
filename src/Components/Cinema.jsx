352ad6d3a2641037a21e110cffd650c1ec760a9b
Sergiu <you@example.com>
2021-03-19 18:50:33 +0200

    Created cinema component

---
+++ src/Components/Cinema.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 0 },
+    items: 1,
+  },
+};
+
+export default function Cinema() {
+  return (
+    <Carousel
+      responsive={responsive}
+      partialVisible={true}
+      showDots={true}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      keyBoardControl={true}
+      transitionDuration={1000}
+    >
+      <div>
+        <img src="https://fakeimg.pl/1600x500/ff0000/" />
+      </div>
+      <div>
+        <img src="https://fakeimg.pl/1600x500/ff3060" />
+      </div>
+      <div>
+        <img src="https://fakeimg.pl/1600x500/bf0000" />
+      </div>
+      <div>
+        <img src="https://fakeimg.pl/1600x500/" />
+      </div>
+    </Carousel>
+  );
+}
--- src/Components/Pages/Newest.jsx
+++ src/Components/Pages/Newest.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Cinema from "../Cinema";
 
 export default function Newest() {
   return (
     <div>
       <h2>Newest Page</h2>
+      <Cinema />
     </div>
   );
 }
