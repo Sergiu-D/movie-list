0e43ba44c9165e86b25554c37c0c98c83946d061
Sergiu <you@example.com>
2021-03-24 12:53:16 +0200

    Fetching discover API

--- src/Components/Pages/Discover.jsx
+++ src/Components/Pages/Discover.jsx
@@ -9,18 +9,22 @@ import MoviePagination from "../MoviePagination";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Discover() {
+  const [pageIndex, setPageIndex] = useState(1);
+
   const { data, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/photos",
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&&page=${pageIndex}`,
     fetcher
   );
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
+  console.log(data);
+
   return (
     <>
       <h2>Discover Page</h2>
-      <MoviePagination data={data} />
+      {/* <MoviePagination data={data} /> */}
     </>
   );
 }
