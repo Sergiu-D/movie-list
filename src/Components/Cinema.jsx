85f0fc4376aaacb02bfcded8e63fe007e726deff
Sergiu <you@example.com>
2021-06-02 20:47:53 +0300

    Added background to movie details component

--- src/Components/Cinema.jsx
+++ src/Components/Cinema.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-import { AutoRotatingCarousel } from "material-auto-rotating-carousel";
-
-// import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
   superLargeDesktop: {
--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -3,7 +3,15 @@ import axios from "axios";
 
 import { useLocation } from "react-router-dom";
 
+//Material-ui
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  bgContainer: {},
+}));
+
 export default function MovieDetails({ match }) {
+  const classes = useStyles();
   const [titleDetails, setTitleDetails] = useState([]);
 
   const location = useLocation();
@@ -48,9 +56,45 @@ export default function MovieDetails({ match }) {
   //   console.log("Title Bg: ", movieBg);
   return (
     <div>
-      {title}
+      {/* Background container */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100vh",
+          zIndex: -5,
+        }}
+      >
+        {/* Background image */}
+        <img
+          src={movieBg}
+          alt={`${title} poster`}
+          style={{
+            width: "100%",
+            height: "100vh",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        />
 
-      <img src={movieBg} alt={`${title} poster`} />
+        {/* Background overlay */}
+        <div
+          style={{
+            position: "inherit",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100vh",
+            background:
+              "radial-gradient(circle, rgba(0,54,77,.8) 10% , rgba(0,11,15,0.98) 70%)",
+          }}
+        ></div>
+      </div>
+      {/* End background container */}
+
+      {title}
     </div>
   );
 }
